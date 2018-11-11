349f2b405c1da47e8378343f060b483227476463
cequella <pedro.oliveira.demolay@gmail.com>
2018-11-11 16:21:27 -0300

    Created login page

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 
+import { MdcTopAppBarModule,
+	 MdcIconModule,
+	 MdcDrawerModule,
+	 MdcListModule,
+	 MdcTextFieldModule,
+	 MdcFormFieldModule,
+	 MdcButtonModule
+       } from '@angular-mdc/web';
+
 @NgModule({
     declarations: [],
     imports: [
+	MdcTopAppBarModule,
+	MdcIconModule,
+	MdcDrawerModule,
+	MdcListModule,
+	MdcTextFieldModule,
+	MdcFormFieldModule,
+	MdcButtonModule
     ],
     exports: [
+	MdcTopAppBarModule,
+	MdcIconModule,
+	MdcDrawerModule,
+	MdcListModule,
+	MdcTextFieldModule,
+	MdcFormFieldModule,
+	MdcButtonModule
     ]
 })
 export class MaterialModule { }
---
+++ src/app/root-page/root-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root-page',
+    templateUrl: './root-page.component.html',
+    styleUrls: ['./root-page.component.scss']
+})
+export class RootPageComponent implements OnInit {
+    menu =[
+	{ icon: "home", label: "Home", path: "/" }
+    ];
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
